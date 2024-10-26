aefbb34b874f53a9d8724bd2fd94e5f5dc5a73ca
Hamze Al Maliky <hamzealmaliky@gmail.com>
2024-10-26 21:20:01 +0200

    removed unnecessary code-blocks

--- src/main/java/io/hamza/github/listener/BomberListener.java
+++ src/main/java/io/hamza/github/listener/BomberListener.java
@@ -2,82 +2,155 @@ package io.hamza.github.listener;
 
 import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import io.hamza.github.main.Main;
+import io.hamza.github.utilities.CreateItemMeta;
 import io.hamza.github.utilities.WorldUtilitie;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.HashMap;
+import java.util.List;
 
-public class BomberListener implements org.bukkit.event.Listener {
+public class BomberListener implements Listener {
 
     private final Main plugin;
     private final HashMap<Player, Location> playerLocationHashMap;
-    private final HashMap<Player, Boolean> isWearingElytra;
     private final HashMap<Player, Long> playerLastTimeHashMap;
     private final HashMap<Player, Integer> flyingSpeedHashMap;
 
+    private final String nukeItemName = "§cNuke";
+    private ItemStack nuke;
+    private ItemStack elytra;
+    private BossBar bar;
+    private int speed = 20;
+    private int maxSpeed = 40;
+
+    private Player player;
 
     public BomberListener(Main plugin) {
         this.plugin = plugin;
         this.playerLocationHashMap = new HashMap<>();
         this.playerLastTimeHashMap = new HashMap<>();
         this.flyingSpeedHashMap = new HashMap<>();
-        this.isWearingElytra = new HashMap<>();
+    }
+
+    @EventHandler
+    public void playerJoinEvent(PlayerJoinEvent event) {
+        CreateItemMeta createItemMeta = new CreateItemMeta();
+        player = event.getPlayer();
+        bar = Bukkit.createBossBar(
+                 "0B/s",
+                BarColor.WHITE,
+                BarStyle.SOLID
+        );
+
+        bar.addPlayer(player);
+
+        nuke = createItemMeta.createItem(Material.FIREWORK_ROCKET, nukeItemName, List.of("§4Do a 20er Bomb"), true);
+        elytra = createItemMeta.createItem(Material.ELYTRA, "§l§eBomber", List.of(""), true);
+
+
+        player.getInventory().setItem(0, nuke);
+        player.getInventory().setChestplate(elytra);
+        player.sendMessage("meow!");
+    }
+
+    @EventHandler
+    public void inventoryClickEvent(InventoryClickEvent event) {
+        if (event.getClickedInventory() == null) return;
+
+        player = (Player) event.getWhoClicked();
+
+        ItemStack item = event.getCurrentItem();
+
+        if (item != null && item.isSimilar(elytra) || item.isSimilar(nuke)) {
+            event.setCancelled(true);
+        } else {
+            event.setCancelled(false);
+        }
+    }
+
+    public void setBossBar(int blockSpeed) {
+        bar.setTitle(blockSpeed + "b/s");
+        bar.setColor(BarColor.GREEN);
+
+        if (blockSpeed >= 18) {
+            bar.setColor(BarColor.YELLOW);
+        }
+
+        if (blockSpeed >= 33) {
+            bar.setColor(BarColor.RED);
+        }
+        try {
+            bar.setProgress((double) blockSpeed / maxSpeed);
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 
     @EventHandler
     public void playerQuitEven(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
+        player = event.getPlayer();
         playerLastTimeHashMap.remove(player);
         flyingSpeedHashMap.remove(player);
         playerLocationHashMap.remove(player);
-        isWearingElytra.remove(player);
+    }
+
+    public boolean isWearingElytra(Player player) {
+        if (player.getInventory().getChestplate().isSimilar(elytra)) {
+            return true;
+        }
+        return false;
     }
 
     @EventHandler
-    public void PlayerArmorChangeEvent(PlayerArmorChangeEvent event) {
-        Player player = event.getPlayer();
-        if (!isWearingElytra.containsKey(event.getPlayer())) {
-            isWearingElytra.put(event.getPlayer(), false);
+    public void openBombBay(PlayerInteractEvent event) {
+        player = event.getPlayer();
+        if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+            ItemStack itemInHand = player.getInventory().getItemInMainHand();
+
+            if (itemInHand.isSimilar(nuke)) {
+                player.sendMessage("Meow!");
+                throwBomb(player);
+            }
+
         }
 
-        if (event.getSlotType() == PlayerArmorChangeEvent.SlotType.CHEST && event.getNewItem().equals(ItemStack.of(Material.ELYTRA))) {
-            isWearingElytra.put(event.getPlayer(), true);
-        } else {
-            player.sendMessage("FALSE");
-            isWearingElytra.put(event.getPlayer(), false);
-        }
     }
 
-
-    @EventHandler
-    public void PlayerMoveEvent(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
+    public void throwBomb(Player player) {
         int debug = 0;
 
-        if (!isWearingElytra.containsKey(player)) {
-            isWearingElytra.put(event.getPlayer(), false);
-        }
-
-        if (isWearingElytra.get(player) && player.isGliding() && flyingSpeedHashMap.get(player) >= 22) {
+        if (isWearingElytra(player) && player.isGliding() && flyingSpeedHashMap.get(player) >= speed) {
             player.getWorld().spawnEntity(player.getLocation(), EntityType.ARROW);
             debug++;
             player.sendMessage(debug + "");
         }
     }
 
+
     @EventHandler
     public void checkBlocksPerSecond(PlayerMoveEvent event) {
+        if (!flyingSpeedHashMap.containsKey(player)) {
+            flyingSpeedHashMap.put(player, 0);
+        }
+
         WorldUtilitie worldUtilitie = new WorldUtilitie();
-        Player player = event.getPlayer();
+        player = event.getPlayer();
         Location location = player.getLocation();
 
         long currentTime = System.currentTimeMillis() + 1000;
@@ -86,7 +159,7 @@ public class BomberListener implements org.bukkit.event.Listener {
                 playerLocationHashMap, playerLastTimeHashMap, flyingSpeedHashMap);
 
         if (bps != -1) {
-            player.sendMessage(bps + "B/s");
+            setBossBar((int) bps);
         }
     }
 
@@ -94,7 +167,16 @@ public class BomberListener implements org.bukkit.event.Listener {
     public void ProjectileHit(ProjectileHitEvent event) {
         if (event.getEntity().getType() == EntityType.ARROW) {
             Entity entity = event.getEntity();
-            entity.getWorld().createExplosion(entity.getLocation(), 10, true, true);
+            entity.getWorld().createExplosion(entity.getLocation(), 20, true, true);
+            event.getEntity().remove();
+        }
+    }
+
+    @EventHandler
+    public void throwSnowBall(ProjectileHitEvent event) {
+        if (event.getEntity().getType() == EntityType.SNOWBALL) {
+            Entity entity = event.getEntity();
+            entity.getWorld().createExplosion(entity.getLocation(), 8, true, true);
             event.getEntity().remove();
         }
     }
--- src/main/java/io/hamza/github/utilities/CreateItemMeta.java
+++ src/main/java/io/hamza/github/utilities/CreateItemMeta.java
@@ -1,4 +1,25 @@
 package io.hamza.github.utilities;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
 public class CreateItemMeta {
+
+    public ItemStack createItem(Material material, String displayName, List<String> lore, boolean enchantmentGlintOverride) {
+
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(displayName);
+        itemMeta.setLore(lore);
+        itemMeta.setEnchantmentGlintOverride(enchantmentGlintOverride);
+
+        itemStack.setItemMeta(itemMeta);
+
+        return itemStack;
+    }
+
 }
